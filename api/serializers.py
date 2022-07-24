bf203f3dfd0a9b7b74f93b76d8c6b5dbfecbee15
PdcShoun <pdc.pachara@gmail.com>
2022-07-24 12:42:08 +0700

    first commit

---
+++ api/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_permit', 
+        'votes_to_skip', 'create_at')
+
+
